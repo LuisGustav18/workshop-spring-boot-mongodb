fcb277f1b6a50885bc9d0cc998e27414e9088b8b
LuisGustav18 <luis0gustavo43@gmail.com>
2025-12-10 23:00:49 -0300

    create post

---
+++ src/main/java/com/gustav/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.gustav.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gustav.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
