0ebcc25c703bdf5b51654be3b7c03d304e896315
LuisGustav18 <luis0gustavo43@gmail.com>
2025-12-12 20:51:07 -0300

    adding comments in post

--- src/main/java/com/gustav/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustav/workshopmongo/config/Instantiation.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.TimeZone;
 
 import com.gustav.workshopmongo.dto.AuthorDTO;
+import com.gustav.workshopmongo.dto.CommentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -41,8 +42,15 @@ public class Instantiation implements CommandLineRunner{
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-		
-		postReposiroty.saveAll(Arrays.asList(post1, post2));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
+
+        postReposiroty.saveAll(Arrays.asList(post1, post2));
 
         maria.getPots().addAll(Arrays.asList(post1, post2));
         userReposiroty.save(maria);
--- src/main/java/com/gustav/workshopmongo/domain/Post.java
+++ src/main/java/com/gustav/workshopmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.gustav.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import com.gustav.workshopmongo.dto.AuthorDTO;
+import com.gustav.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -20,8 +23,9 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
-	
-	public Post() {
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post() {
 		
 	}	
 
@@ -74,7 +78,15 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
-	@Override
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
+    @Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
