83783a824f60ef9309500c668826c7b37eb21e0c
LuisGustav18 <luis0gustavo43@gmail.com>
2025-12-12 21:45:27 -0300

    adding search by title in post

--- src/main/java/com/gustav/workshopmongo/resources/PostResource.java
+++ src/main/java/com/gustav/workshopmongo/resources/PostResource.java
@@ -1,13 +1,14 @@
 package com.gustav.workshopmongo.resources;
 
 import com.gustav.workshopmongo.domain.Post;
+import com.gustav.workshopmongo.resources.util.URL;
 import com.gustav.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.HTMLEditorKit;
+import java.util.List;
 
 
 @RestController
@@ -21,5 +22,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+        text = URL.decodParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
